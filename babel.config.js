e82630ea3f141861738f718cee9f8fdfdd815206
Luiz Ferreira <luiz.ferreira@outlook.com>
2020-04-10 14:14:11 -0300

    Revert "Correção do plugin-transform-runtime"
    This reverts commit f0c0f3d1c824bfc232fba52e9a7bace2e3e874e0.

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
-module.exports = {
+module.export = {
   presets: [
     '@babel/preset-env',
-    '@babel/preset-react',
+    '@babel/preset-react'
   ],
-  plugins: [
-    '@babel/plugin-transform-runtime'
-  ]
 };
--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         }
       }
     ]
